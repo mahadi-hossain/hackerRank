0ee06214df5f38d7f813ffac3bbc719e484065bf
mahadi-hossain <mahadihossainpvt@gmail.com>
2021-12-07 02:39:19 +0600

    Update super-digit.hs

--- super-digit.hs
+++ super-digit.hs
@@ -31,3 +31,39 @@ main = do
   print $ superDigit ( toI (head wds)) (toI (last wds))
   
   
+  
+Good code .....
+
+
+module Main
+  where
+  
+import Control.Monad ( join )
+import Data.Char (digitToInt)
+import System.Environment
+import Data.List
+import System.IO
+
+sumDigits :: String  -> String 
+sumDigits res 
+    | (length res) == 1 = res
+    | otherwise  = sumDigits ( show $ foldr (\chac acc -> acc+digitToInt chac) 0  res)
+
+superDigit :: Integer -> Integer  -> Int   
+superDigit n k =
+    let stringDigit = show n
+        replicateAndSumDigit1sRes =  sumDigits $ join $ replicate (fromIntegral k) (sumDigits stringDigit)
+      in  read replicateAndSumDigit1sRes 
+      
+      
+toI  x =  read x :: Integer
+
+main :: IO ()
+main = do
+  arg <- getLine
+  let wds = words arg
+  print $ superDigit ( toI (head wds)) (toI (last wds))
+  
+  
+  
+  
